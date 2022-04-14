c4cb55753d10f709de84d2d183725c478f5fc2c8
Eddie Webb <ollitech@gmail.com>
2022-04-14 16:04:54 -0400

    generated invalid config locally

--- .circleci/dynamic/index.js
+++ .circleci/dynamic/index.js
@@ -1,6 +1,9 @@
 
 const CircleCI = require("@circleci/circleci-config-sdk");
 const fs = require('fs');
+const lineReader = require('line-reader');
+
+
 // Instantiate new Config
 const myConfig = new CircleCI.Config();
 // Create new Workflow
@@ -12,35 +15,49 @@ myConfig.addWorkflow(myWorkflow);
 // and do not need to be added to the config separately
 const nodeExecutor = new CircleCI.executor.DockerExecutor('cimg/node:lts');
 
-// Create Job and add it to the config
-const nodeTestJob = new CircleCI.Job('node-test', nodeExecutor);
-myConfig.addJob(nodeTestJob);
 
-// Add steps to job
-nodeTestJob
+
+
+lineReader.eachLine('changedpaths.txt', function(line, last) {
+  console.log(`Line from file: ${line}`);
+
+  // Create Job and add it to the config
+  const nodeTestJob = new CircleCI.Job(`job-${line}`, nodeExecutor);
+  myConfig.addJob(nodeTestJob);
+
+  // Add steps to job
+  nodeTestJob
   .addStep(new CircleCI.commands.Checkout())
   .addStep(
     new CircleCI.commands.Run({
-      command: 'npm install',
-      name: 'NPM Install',
+      command: `echo ${line}`,
+      name: `${line}`,
     }),
   )
-  .addStep(
-    new CircleCI.commands.Run({
-      command: 'npm run test',
-      name: 'Run tests',
-    }),
-  );
 
-// Add Jobs to Workflow
-myWorkflow.addJob(nodeTestJob);
+  // Add Jobs to Workflow
+  myWorkflow.addJob(nodeTestJob);
 
-// The `stringify()` function on `CircleCI.Config` will return the CircleCI YAML equivalent.
-const MyYamlConfig = myConfig.stringify();
 
-fs.writeFile('./dynamicConfig.yml', MyYamlConfig, (err) => {
-  if (err) {
-    console.log(err);
-    return
+
+  if(last) {
+    console.log('Last line printed.');
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+    // The `stringify()` function on `CircleCI.Config` will return the CircleCI YAML equivalent.
+    const MyYamlConfig = myConfig.stringify();
+    
+    fs.writeFile('./dynamicConfig.yml', MyYamlConfig, (err) => {
+      if (err) {
+        console.log(err);
+        return
+      }
+    })
   }
-})
+});
+
+
+
+
+
+
