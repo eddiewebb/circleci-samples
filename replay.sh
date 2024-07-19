1d5baa0b4109e7c1e7f827317db146804661771b
Eddie Webb <ollitech@gmail.com>
2024-07-19 08:53:32 -0400

    rename var

--- replay.sh
+++ replay.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 COMMIT_COUNT=3
-FOUR_OLD=$(git log -n$((COMMIT_COUNT+1)) --oneline | tail -1 | cut -f1 -d" ")
+COMMIT_BEFORE_TARGET=$(git log -n$((COMMIT_COUNT+1)) --oneline | tail -1 | cut -f1 -d" ")
 BRANCH_NAME=load
 git branch -d ${BRANCH_NAME} || echo "No stale branch locally"
 git push -d origin ${BRANCH_NAME}|| echo "No stale branch remotely"
-git branch ${BRANCH_NAME} ${FOUR_OLD}
+git branch ${BRANCH_NAME} ${COMMIT_BEFORE_TARGET}
 git push origin ${BRANCH_NAME}
 
 #Remote branch exists N+1 commits ago, ready to replay.
